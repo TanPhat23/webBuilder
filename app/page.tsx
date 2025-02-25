e52d5e123d682a17b4a689b586c15e2e83ece2bb
Ben2811 <luanchuong51@gmail.com>
2025-02-25 20:19:59 +0700

    add Navbar Footer Main

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 import React from "react";
-
+import Navbar from "@/components/landingpage/Navbar";
+import Footer from "@/components/landingpage/Footer";
+import Main from "@/components/landingpage/Main";
 function LandingPage() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col min-h-screen overflow-y-auto">
+      <Navbar />
+      <div className="flex-1">
+        <Main />
+      </div>
+      <Footer />
+    </div>
+  );
 }
+
 
 export default LandingPage;
---
+++ components/landingpage/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-700 text-white text-sm py-2 px-5 w-full ">
+      © Copyright Website 2025
+    </footer>
+  );
+};
+
+export default Footer;
